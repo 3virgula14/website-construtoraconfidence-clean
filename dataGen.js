34189941a7d70bb24f04bf553ae65de592a02b61
3virgula14 <3virgula14de96@gmail.com>
2022-10-25 09:33:43 -0300

    dataGen mdsession feature

--- dataGen.js
+++ dataGen.js
@@ -10,6 +10,15 @@ fs.readdir("public/portfolio_items", (err, files) => {
             fileContentArr+= data+",";
         })
         let finaljson = JSON.parse(`{"portfolio":[${fileContentArr.slice(0, -1)}]}`)
+        finaljson['portfolio'] = finaljson['portfolio'].map( (j, i) => {
+          return j['sessions'] && j['sessions'].map( (s, ii) => {
+            if(s['mdsession'].length<10){
+              s['mdsession'] += `![](${s['foto']})`
+              s['mdsession'] += `\n${s['shortstory']}\n${s['tailstory']}`
+            }
+            return s
+          })
+        })
         fs.writeFile("public/data.json", JSON.stringify(finaljson), (err) =>{})
     }
   })
