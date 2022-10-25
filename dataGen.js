548a4fbb1e10aeae763037902d99925a5551dcef
3virgula14 <3virgula14de96@gmail.com>
2022-10-25 09:35:50 -0300

    1

--- dataGen.js
+++ dataGen.js
@@ -11,13 +11,14 @@ fs.readdir("public/portfolio_items", (err, files) => {
         })
         let finaljson = JSON.parse(`{"portfolio":[${fileContentArr.slice(0, -1)}]}`)
         finaljson['portfolio'] = finaljson['portfolio'].map( (j, i) => {
-          return j['sessions'] && j['sessions'].map( (s, ii) => {
+          j['sessions'] = j['sessions'].map( (s, ii) => {
             if(s['mdsession'].length<10){
               s['mdsession'] += `![](${s['foto']})`
               s['mdsession'] += `\n${s['shortstory']}\n${s['tailstory']}`
             }
             return s
           })
+          return j
         })
         fs.writeFile("public/data.json", JSON.stringify(finaljson), (err) =>{})
     }
