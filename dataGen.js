d3a3dfeb3c859a1cc054431cff1d2ed921fbb6f2
3virgula14 <3virgula14de96@gmail.com>
2022-10-25 00:59:26 -0300

    dataGen refact

--- dataGen.js
+++ dataGen.js
@@ -1,15 +1,15 @@
 import fs from 'fs'
 
-fs.readdir(__dirname+"/public/portfolio_items", (err, files) => {
+fs.readdir("public/portfolio_items", (err, files) => {
     if (err)
       console.log(err);
     else {
         let fileContentArr = []
         files.forEach(file => {
-            const data = fs.readFileSync(__dirname+"/public/portfolio_items/"+file, 'utf8');
+            const data = fs.readFileSync("public/portfolio_items/"+file, 'utf8');
             fileContentArr+= data+",";
         })
         let finaljson = JSON.parse(`{"portfolio":[${fileContentArr.slice(0, -1)}]}`)
-        fs.writeFile(__dirname+"/public/data.json", JSON.stringify(finaljson), (err) =>{})
+        fs.writeFile("public/data.json", JSON.stringify(finaljson), (err) =>{})
     }
   })
